2f015f5223d8f05dd9c786487c0c3484fca76b0a
Reticent Facade <lipisharma2911@gmail.com>
2023-09-10 21:42:44 +0530

    added env variables

--- void_lib/tests/archive_test.rs
+++ void_lib/tests/archive_test.rs
@@ -1,11 +1,16 @@
 use void_lib::archive_operations::archive_dir::archive_dir;
+use std::env;
+use dotenv::dotenv;
 
 #[test]
 pub fn test_archive_dir() {
-    let source_dir = "/Users/Reticent/Desktop/Work/ProjectWorkspace/github/termvoid/void_lib/tests/test_data/archive_dir/source_dir";
-    let destination_dir = "/Users/Reticent/Desktop/Work/ProjectWorkspace/github/termvoid/void_lib/tests/test_data/archive_dir/destination_dir";
+    // Loading environment variables from .env file:
+    dotenv().ok();
+    
+    let source_dir = env::var("SOURCE_DIR").expect("SOURCE_DIR not set in .env");
+    let destination_dir = env::var("DESTINATION_DIR").expect("DESTINATION_DIR not set in .env");
 
-    match archive_dir(source_dir, destination_dir) {
+    match archive_dir(&source_dir, &destination_dir) {
         Ok(_) => assert!(true),
         Err(e) => println!("Error: {}", e),
     }
