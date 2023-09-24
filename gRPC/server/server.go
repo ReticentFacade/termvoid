bcadd62e4571dbb5d1797b4b269fb5b42db01a70
Reticent Facade <lipisharma2911@gmail.com>
2023-09-24 14:41:34 +0530

    grpc service initiated

---
+++ gRPC/server/server.go
@@ -0,0 +1,13 @@
+package main
+
+import context "context"
+
+const (
+	port = "9000"
+)
+
+type server struct {
+	// Implement gRPC methods here
+}
+
+func (s *server) UploadFile(ctx context.Context)
