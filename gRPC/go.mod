455ccebad7168a5c1056921ccccfa6c34d64699c
Reticent Facade <lipisharma2911@gmail.com>
2023-09-24 20:26:59 +0530

    grpc initiated

---
+++ gRPC/go.mod
@@ -0,0 +1,28 @@
+module void
+
+go 1.21.1
+
+require (
+	google.golang.org/grpc v1.58.2
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/glog v1.1.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.18.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	google.golang.org/genproto v0.0.0-20230913181813-007df8e322eb // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230822172742-b8732ec3820d // indirect
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/spf13/cobra v1.7.0
+	golang.org/x/net v0.15.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230920204549-e6e6cdab5c13 // indirect
+)
--- gRPC/server/server.go
+++ gRPC/server/server.go
@@ -1,6 +1,14 @@
 package main
 
-import context "context"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/ReticentFacade/termvoid/gRPC/proto"
+	"google.golang.org/grpc"
+)
 
 const (
 	port = "9000"
@@ -10,4 +18,24 @@ type server struct {
 	// Implement gRPC methods here
 }
 
-func (s *server) UploadFile(ctx context.Context)
+func (s *server) UploadFile(ctx context.Context, request *pb.FileContent) (*pb.FileResponse, error) {
+	fmt.Println("UploadFile called...")
+
+	// File upload logic here:
+	return &pb.FileResponse{}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterFileServiceServer(grpcServer, &server{}) // Register the service implementation
+
+	log.Printf("Server listening on port %s...\n", port)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
